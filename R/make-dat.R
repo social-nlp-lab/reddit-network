88d8e0bfabf198b0c7b7f115b506bde0b665efb5
layla b <lab218@miami.edu>
2023-02-17 14:00:39 -0500

    update ignore

--- R/make-dat.R
+++ R/make-dat.R
@@ -4,7 +4,6 @@
 
 library(tidyverse)
 library(jsonlite)
-library(igraph)
 
 dat_path <- "~/OneDrive - Drexel University/Social_NLP_Lab/Datasets/Reddit Drug Data- RAW - DNT/Central/data Dec 1st 2022/data 2018_1_1 to 2022_7_30_"
 subs <- c("opiates", "cocaine", "stims", "benzodiazepines")
@@ -25,5 +24,9 @@ posts <- subs |> map(\(x){
 }
 ) |> list_rbind()
 
-# scatter plot between score and number of posts
+#########################
+# using reddit api to get active users
 
+library(httr)
+
+x <- read_json("https://api.pushshift.io/reddit/search/comment?since=30d&subreddit=science&aggs=author&agg_size=500")
