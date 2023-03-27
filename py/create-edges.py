21c8f256711ced61a9c289d2bd00784f4d0e61b6
layla b <lab218@miami.edu>
2023-03-27 10:27:00 -0400

    add from col to edge list

--- py/create-edges.py
+++ py/create-edges.py
@@ -23,9 +23,32 @@ for date in dates:
             names = user_dict.keys()
             subs = user_dict.values()
             user_subs = pd.concat([user_subs,pd.DataFrame({'name': names, 'subreddit': subs})])
+# reset index
+user_subs = user_subs.reset_index(drop=True)
+# add 'from' column
+drug_subs = ['LSD', 'stims', 'opiates', 'cocaine', 'benzodiazepines', 'shrooms', 'ambien', 'Autoflowers', 'cannabis', 'Drugs', 'MDMA', 'Psychedelics']
+# subreddit type is list, iterate through list to find if any are in drug_subs
+def find_druggsub(row):
+    matches = []
+    for sub in row:
+        if sub in drug_subs:
+            matches.append(sub)
+            return matches
+    return None
+user_subs['from'] = user_subs['subreddit'].apply(find_druggsub)
+
+# nan values are users who only commented but did not post
+# get freq
+user_subs['from'].value_counts(dropna=False)
+# drop nan
+user_subs = user_subs.dropna()
+# 8321 users and their subs
+
+
 
 # expand subreddit column
 user_subs = user_subs.explode('subreddit')
+# 325401 rows
 # take unique values
 user_subs = user_subs.drop_duplicates()
 #236,962 
